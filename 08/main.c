0901ca8cc03499374713ace71c72e9f0caf5326c
atimangel <atimangel@naver.com>
2024-05-24 10:04:14 +0900

    08 coding style

--- 08/main.c
+++ 08/main.c
@@ -11,14 +11,13 @@ MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read (struct file *fp, char __user *user, size_t size,
 		loff_t *offs);
-
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 		loff_t *offs);
 
 static struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
-	.read = &myfd_read,
-	.write = &myfd_write
+	.read = myfd_read,
+	.write = myfd_write
 };
 
 static struct miscdevice myfd_device = {
@@ -27,14 +26,13 @@ static struct miscdevice myfd_device = {
 	.fops = &myfd_fops
 };
 
-char str[PAGE_SIZE];
+static char str[PAGE_SIZE];
 
 static int __init myfd_init (void)
 {
-	int retval = 0;
+	int retval;
 
 	retval = misc_register(&myfd_device);
-
 	return retval;
 }
 
@@ -51,7 +49,7 @@ ssize_t myfd_read (struct file *fp, char __user *user, size_t size,
 	ssize_t retval = 0;
 	char *tmp;
 
-	tmp = kmalloc(sizeof(char) * PAGE_SIZE, GFP_KERNEL);
+	tmp = kmalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!tmp) {
 		retval = -ENOMEM;
 		goto out;
@@ -59,11 +57,11 @@ ssize_t myfd_read (struct file *fp, char __user *user, size_t size,
 
 	for (t = strlen(str), i = 0; t > 0; t--, i++)
 		tmp[i] = str[t - 1];
-	
-	tmp[i] = 0x0;
+
+	tmp[i] = '\0';
 	retval = simple_read_from_buffer(user, size, offs, tmp, i);
 	kfree(tmp);
-	
+
 out:
 	return retval;
 }
@@ -74,11 +72,10 @@ ssize_t myfd_write (struct file *fp, const char __user *user, size_t size,
 	ssize_t res;
 
 	res = simple_write_to_buffer(str, size, offs, user, size);
-	str[res] = 0x0;
+	str[res] = '\0';
 
 	return res;
 }
 
 module_init(myfd_init);
 module_exit(myfd_cleanup);
-
